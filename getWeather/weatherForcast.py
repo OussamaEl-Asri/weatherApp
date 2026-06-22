5b4f3a98a03f59c754ac679d5cf8a983cd94fd02
trool maroc <troolmaroc1@gmail.com>
2026-06-22 16:12:02 +0100

    feat(api): support date range queries for weather data

--- getWeather/weatherForcast.py
+++ getWeather/weatherForcast.py
@@ -1,6 +1,6 @@
 from model import model
 from typing import Any
-from datetime import date
+from datetime import date, timedelta
 import requests
 import json
 import logging
@@ -24,32 +24,44 @@ def data_modeling(weathers: dict[str, Any]) -> list[model.WeatherResponse]:
     return weather_response
 
 
-def get_range_date(endpoint: str, start_date: date, end_date: date):
-    pass
+def get_date_range(
+        endpoint: str, 
+        start_date: date, 
+        end_date: date) ->  model.ResponseModel:
+    weather_response : list[model.WeatherResponse] = []
 
+    while start_date <= end_date:
+        response = requests.get(endpoint + f"&dt={start_date}")
+        
+        if response.status_code != 200:
+            return model.ResponseModel(status_code=response.status_code, error=response.text)
+        res = response.json()
+        weather_response.extend(data_modeling(res))
+        start_date += timedelta(days=1)
+
+    return model.ResponseModel(status_code=200, message=weather_response)
+        
 def get_weather(
         weather_input: model.WeatherInfoRequest, 
         base_url: str, 
         api_key: str | None = None
     ) -> model.ResponseModel:
 
+    endpoint_url = base_url + f"/forecast.json?key={api_key}"
 
     if weather_input.city:
-        query_parameter = weather_input.city
+        endpoint_url += f"&q={weather_input.city}"
     
     elif weather_input.lat_long:
-        query_parameter = str(weather_input.lat_long[0]) + "," + \
-                          str(weather_input.lat_long[1])
+        endpoint_url += f"&q={weather_input.lat_long[0]},{weather_input.lat_long[1]}"
     
     elif weather_input.zip_code:
-        query_parameter = str(weather_input.zip_code)
+        endpoint_url += f"&q={weather_input.zip_code}"
     
     if weather_input.start_date and weather_input.end_date:
-        endpoint_url = base_url + \
-            f"/future.json?key={api_key}&q={query_parameter}&dt={weather_input.start_date}"
+        return get_date_range(endpoint_url, weather_input.start_date, weather_input.end_date)
     else:
-        endpoint_url = base_url + \
-        f"/forecast.json?key={api_key}&q={query_parameter}&days={weather_input.days_forecast}"
+        endpoint_url += f"&days={weather_input.days_forecast}"
     
     response = requests.get(endpoint_url)
     
@@ -59,4 +71,3 @@ def get_weather(
     else:
         data = data_modeling(response.json())
         return model.ResponseModel(status_code= 200, message=data)
-
